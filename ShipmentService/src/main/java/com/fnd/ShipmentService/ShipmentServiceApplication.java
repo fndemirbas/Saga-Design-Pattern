189949450ed9231850cfa17a17677a78d0084c89
Fatma Nur Demirbaş <54847231+fndemirbas@users.noreply.github.com>
2023-07-31 22:32:34 +0300

    Initial commit

---
+++ ShipmentService/src/main/java/com/fnd/ShipmentService/ShipmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fnd.ShipmentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipmentServiceApplication.class, args);
+	}
+
+}
